20562fa7f5dc876e788077a229d39fcdb08c3903
Madison Brooks <madison@brightly.com>
2022-11-22 21:07:27 -0500

    Array of Components Render

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,19 +1,56 @@
 import React from "react";
 import "./App.css";
+import testData from "./TestData";
 
-class GitHubCards extends React.Component {
-  // render function is the only required function for a React Component
-  // it returns the virtual DOM description of the component
+const CardList = (props) => (
+  <div>
+    {/* Mapping the testData object into React Components, 
+      this returns an array of card components that then get rendered in the DOM*/}
+    {testData.map((profile) => (
+      <CardComponent {...profile} />
+    ))}
+    {/* The code below will do the job one at a time but that is not what we want
+    <CardComponent {...testData[0]} />
+    */}
+  </div>
+);
+
+// Note that for simplicity the classes are all being defined within this one file :)
+//    - I do want to break this out
+class CardComponent extends React.Component {
   render() {
-    return <div className="App-header">{this.props.title}</div>;
+    const profile = this.props;
+    return (
+      <div className="card">
+        <img src={profile.avatar_url} alt="profile"></img>
+        <div className="info">
+          {/* Below is an example of in-line styling
+                NOTE: it needs to be treated like an object
+                https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/style-prop-object.md
+          */}
+          <div style={{ fontWeight: "bold" }}>{profile.name}</div>
+          <div>{profile.company}</div>
+        </div>
+      </div>
+    );
   }
 }
 
-// Defining the same thing but with a variable
-// const GitHubCards = ({ title }) => <div className="App-header">{title}</div>;
+class BaseComponent extends React.Component {
+  // render function is the only required function for a React Component
+  // it returns the virtual DOM description of the component
+  render() {
+    return (
+      <div className="app">
+        <div className="header">{this.props.title}</div>
+        <CardList />
+      </div>
+    );
+  }
+}
 
 function App() {
-  return <GitHubCards title="Madison's First React App" />;
+  return <BaseComponent title="Madison's First React App" />;
 }
 
 export default App;
---
+++ my-first-app/src/TestData.js
@@ -0,0 +1,46 @@
+// Information from https://api.github.com/users
+const testData = [
+  {
+    key: 1,
+    name: "mojombo",
+    avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+    company: "GitHub",
+  },
+  {
+    key: 2,
+    name: "defunkt",
+    avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
+    company: "GitHub",
+  },
+  {
+    key: 3,
+    name: "pjhyett",
+    avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
+    company: "GitHub",
+  },
+  {
+    key: 4,
+    name: "wycats",
+    avatar_url: "https://avatars.githubusercontent.com/u/4?v=4",
+    company: "GitHub",
+  },
+  {
+    key: 5,
+    name: "ezmobius",
+    avatar_url: "https://avatars.githubusercontent.com/u/5?v=4",
+    company: "GitHub",
+  },
+  {
+    key: 6,
+    name: "ivey",
+    avatar_url: "https://avatars.githubusercontent.com/u/6?v=4",
+    company: "GitHub",
+  },
+  {
+    key: 7,
+    name: "evanphx",
+    avatar_url: "https://avatars.githubusercontent.com/u/7?v=4",
+    company: "GitHub",
+  },
+];
+export default testData;
